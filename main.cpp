db0de1dce04c4a1ad0817d2c5163a17843906504
Kapuh <anssi224@gmail.com>
2018-02-08 11:55:38 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sekuntti;
+    int minuutti;
+
+    cout << "This is Stopwatch" << endl << endl;
+    cout << "Tahan merkkaa sekuntteja" << endl;
+    cin >> sekuntti;
+    cout << endl << endl;
+
+    cout << "Tahan merkkaa minuutteja" << endl;
+    cin >> minuutti;
+    cout << endl << endl;
+
+    cout << minuutti << " minuuttia, Hiukan tulostuksessa viela vikaa LUL";
+    return 0;
+}
